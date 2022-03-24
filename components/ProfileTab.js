d9f2c8f2cdeaa4047c6985c50ba9325f65c5102f
starwalker00 <aaronwalkerup@gmail.com>
2022-03-24 09:19:12 +0100

    dev: profile tab in component

--- components/ProfileTab.js
+++ components/ProfileTab.js
@@ -1,33 +1,29 @@
-import Layout from '../../components/layout'
-import Sidebar from '../../components/sidebar'
-import PublicationView from '../../components/PublicationView'
-import PageContainer from '../../components/PageContainer'
+
+import PublicationView from './PublicationView'
 
 import {
-  Text,
-  Button,
-  Flex,
-  Box,
-  Spacer,
-  Stack,
-  Select,
-  Container,
-  Center,
-  Spinner
+    Text,
+    Button,
+    Flex,
+    Box,
+    Spacer,
+    Stack,
+    Select,
+    Container,
+    Center,
+    Spinner
 } from '@chakra-ui/react';
 import { Skeleton, SkeletonCircle, SkeletonText } from '@chakra-ui/react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 
 import { gql, useQuery } from "@apollo/client";
-import { initializeApollo, addApolloState } from "../../lib/apolloClient";
-import { namedConsoleLog, prettyJSON } from '../../lib/helpers';
+import { initializeApollo, addApolloState } from "../lib/apolloClient";
+import { namedConsoleLog, prettyJSON } from '../lib/helpers';
 import { BigNumber } from "@ethersproject/bignumber";
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import { DEMO_PROFILE_ID } from '../../lib/config';
+import { DEMO_PROFILE_ID } from '../lib/config';
 import InfiniteScroll from 'react-infinite-scroll-component';
-import ProfileTab from '../../components/ProfileTab';
-
 const GET_PUBLICATIONS = `
   query($request: PublicationsQueryRequest!) {
     publications(request: $request) {
@@ -320,81 +316,92 @@ const GET_PUBLICATIONS = `
     }
   }
 `;
+// responsively style pages listing publications
+function ProfileTab({ constCurrentProfileID }) {
+    // fetch publications of profileID
+    const { loading: loadingPublication, error, data, fetchMore, refetch } = useQuery(
+        gql(GET_PUBLICATIONS),
+        {
+            variables: {
+                request: {
+                    profileId: constCurrentProfileID.toHexString(),
+                    publicationTypes: ['POST', 'COMMENT', 'MIRROR'],
+                    limit: 10
+                },
+            },
+            notifyOnNetworkStatusChange: true
+        });
+    // prettyJSON('publications', publications);
+    const publications = data?.publications?.items || [];
+    const havePublication = Boolean(publications.length);
+    const totalCount = data?.publications?.pageInfo?.totalCount || 0;
+    const haveMorePublication = Boolean(publications.length < totalCount);
+    namedConsoleLog('data?.publications', data?.publications)
 
-function Profile() {
-  // get profileID from params
-  const router = useRouter();
-  // demo profile needed because profileID is undefined on first render
-  // https://www.joshwcomeau.com/react/the-perils-of-rehydration/
-  const profileID = router.query.profileID ?? DEMO_PROFILE_ID;
-  // prettyJSON('profileID', profileID);
-  const constCurrentProfileID = BigNumber.from(profileID);
-
-
-  return (
-    <section>
-      <Tabs isFitted isLazy lazyBehavior="keepMounted">
-        <TabList>
-          <Tab>One</Tab>
-          <Tab>Two</Tab>
-          <Tab>Three</Tab>
-        </TabList>
-
-        <TabPanels>
-          <TabPanel>
-            <p>one!</p>
-          </TabPanel>
-          <TabPanel>
-            <ProfileTab constCurrentProfileID={constCurrentProfileID} />
-          </TabPanel >
-          <TabPanel>
-            <p>three!</p>
-          </TabPanel>
-        </TabPanels >
-      </Tabs >
-      {/* {console.log(publications)} */}
-      < h1 > profile / [id]</h1 >
-    </section >
-  )
+    function fetchMorePublications() {
+        // prettyJSON('publications.length', publications.length);
+        const pageInfoNext = data.publications.pageInfo.next;
+        prettyJSON('pageInfoNext', pageInfoNext);
+        fetchMore({
+            variables: {
+                request: {
+                    profileId: constCurrentProfileID.toHexString(),
+                    publicationTypes: ['POST', 'COMMENT', 'MIRROR'],
+                    limit: 10,
+                    cursor: pageInfoNext
+                },
+            },
+            updateQuery: (prevResult, { fetchMoreResult }) => {
+                namedConsoleLog('prevResult', prevResult);
+                namedConsoleLog('fetchMoreResult', fetchMoreResult);
+                fetchMoreResult.publications.items = [
+                    ...prevResult.publications.items,
+                    ...fetchMoreResult.publications.items
+                ];
+                // remove eventual duplicates, just in case
+                fetchMoreResult.publications.items = [...new Set([...prevResult.publications.items, ...fetchMoreResult.publications.items])]
+                return fetchMoreResult;
+            }
+        });
+    }
+    return (
+        <>
+            <>
+                {
+                    error
+                        ?
+                        <h1>error</h1>
+                        :
+                        <Container
+                            display='flex'
+                            flexDirection='column'
+                            // maxWidth={{ base: '100%', md: '80% ' }}
+                            width={{ base: '100%', md: '80% ' }}
+                        >
+                            <p>{totalCount} publications</p>
+                            <InfiniteScroll
+                                dataLength={publications.length}
+                                next={fetchMorePublications}
+                                hasMore={haveMorePublication}
+                                loader={
+                                    <Center overflow='hidden'>
+                                        <Spinner
+                                            thickness='4px'
+                                            speed='0.65s'
+                                            size='md' />
+                                    </Center>
+                                }
+                                endMessage={<h4>Nothing more to show</h4>}
+                            >
+                                {publications.map((publication) => (
+                                    <PublicationView key={publication.id} publication={publication} />
+                                ))}
+                            </InfiniteScroll>
+                        </Container>
+                }
+            </>
+        </>
+    )
 }
 
-Profile.getLayout = function getLayout(page) {
-  return (
-    <Layout>
-      <Sidebar>
-        <PageContainer>
-          {page}
-        </PageContainer>
-      </Sidebar>
-    </Layout>
-  )
-}
-
-// TODO? : use static generation on useful pages
-
-// export async function getStaticProps({params}) {
-//   prettyJSON('params', params);
-//   const apolloClient = initializeApollo();
-
-//   const constCurrentProfileID = ethers.BigNumber.from(params.profileID);
-//   const result = await apolloClient.query({
-//     query: gql(GET_PUBLICATIONS),
-//     variables: {
-//       request: {
-//         profileId: constCurrentProfileID.toHexString(),
-//         publicationTypes: ['POST', 'COMMENT', 'MIRROR'],
-//       },
-//     },
-//   });
-//   prettyJSON(`GET_PUBLICATIONS OF PROFILE ID ${params.profileID}`, result);
-//   return addApolloState(apolloClient, {
-//     props: { },
-//   });
-// }
-
-// export async function getStaticPaths() {
-//   const paths = []
-//   return {paths, fallback: true }
-// }
-
-export default Profile
+export default ProfileTab
