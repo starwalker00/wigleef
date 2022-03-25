737c1a113cca1e05a6abfd4bd47bc4bbd0416520
starwalker00 <aaronwalkerup@gmail.com>
2022-03-25 11:18:44 +0100

    dev: redirect on empty dynamic routes

--- lib/config.ts
+++
@@ -1,7 +0,0 @@
-import { BigNumber } from "@ethersproject/bignumber";
-
-export const DEMO_PROFILE_ID = BigNumber.from('0x12');
-export const DEMO_PUBLICATION_ID = '0x12-0x02';
-export const UNSET_CONTEXT_PROFILE_ID = BigNumber.from('0x0');
-export const UNSET_CONTEXT_ACCESS_TOKEN = '';
-export const UNSET_CONTEXT_REFRESH_TOKEN = '';
--- next.config.js
+++ next.config.js
@@ -1,9 +1,32 @@
 /** @type {import('next').NextConfig} */
 const removeImports = require('next-remove-imports')({})
 
+const { DEMO_PROFILE_ID } = require('./lib/config')
+const { DEMO_PUBLICATION_ID } = require('./lib/config')
+
 module.exports = removeImports({
   reactStrictMode: true,
   experimental: {
     scrollRestoration: true
+  },
+  // redirect on empty string, do on page side then
+  async redirects() {
+    return [
+      {
+        source: '/profile',
+        destination: '/profile/'.concat(DEMO_PROFILE_ID.toHexString()),
+        permanent: true,
+      },
+      {
+        source: '/timeline',
+        destination: '/timeline/'.concat(DEMO_PROFILE_ID.toHexString()),
+        permanent: true,
+      },
+      {
+        source: '/publication',
+        destination: '/publication/'.concat(DEMO_PUBLICATION_ID),
+        permanent: true,
+      },
+    ]
   },
 })
